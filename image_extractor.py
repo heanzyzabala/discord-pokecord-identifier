bdb8197ead281c2e973f615fed4b576a3827703d
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-04-25 20:53:39 +0800

    Added web scraper for images

--- image_extractor.py
+++ image_extractor.py
@@ -1,4 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-res = requests.get('https://www.serebii.net/pokemon/all.shtml');
+# print('Sending request..')
+# res = requests.get('https://www.serebii.net/pokemon/all.shtml')
+# print('Received response..')
+f = open('sample.txt')
+soup = BeautifulSoup(f.read(), 'html.parser')
+table = soup.select('table.dextable')[0]
+rows = table.select('tr')
+for row in rows:
+    data = row.select('td.fooinfo > a')
+    for d in data:
+        print(data[0])
