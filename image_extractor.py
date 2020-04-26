2897ad9a02f84cf45de015a8a7344e9ce978c6c2
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-04-26 17:15:40 +0800

    Added working image extractor; Added folder structure for downloaded images

--- image_extractor.py
+++ image_extractor.py
@@ -1,14 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
+import shutil
+import re
+import time
 
-# print('Sending request..')
-# res = requests.get('https://www.serebii.net/pokemon/all.shtml')
-# print('Received response..')
-f = open('sample.txt')
-soup = BeautifulSoup(f.read(), 'html.parser')
-table = soup.select('table.dextable')[0]
-rows = table.select('tr')
-for row in rows:
-    data = row.select('td.fooinfo > a')
-    for d in data:
-        print(data[0])
+print('Sending request..')
+res = requests.get('https://www.serebii.net/pokemon/all.shtml')
+print('Received response..')
+soup = BeautifulSoup(res.text, 'html.parser')
+
+paths = soup.findAll('a', href=re.compile("^(/pokemon/.[^/]+)$"), text=re.compile('.*'))
+
+print('Downloading images..')
+t0 = time.time()
+i = 0
+for path in paths:
+    parent = path.parent
+    if parent.name == 'td':
+        i += 1
+        n = '{0:0>3}'.format(i)
+        url = f'https://www.serebii.net/pokemon/art/{n}.png'
+        print(f'Downloading from: {url}')
+        res = requests.get(url, stream=True)
+        if res.status_code == 200:
+            with open(f'resources/images/{n}.png', 'wb') as f:
+                res.raw.decode_content = True
+                shutil.copyfileobj(res.raw, f)
+            pass
+        else:
+            print(f'*****Unable to download from: {url} - {res.status_code}')
+print('Done')
+t1 = '{:2f}'.format((time.time() - t0) / 60)
+print(f'Time elapsed: {t1} minutes')
