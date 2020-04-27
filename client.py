b84ddac94fff10642da1f93e98003ebc7cbf6da2
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-04-27 22:14:36 +0800

    Added discord client

---
+++ client.py
@@ -0,0 +1,30 @@
+import os
+import discord
+import utility
+from dotenv import load_dotenv
+load_dotenv()
+
+client = discord.Client()
+context = utility.load()
+
+TOKEN = os.getenv('TOKEN')
+
+
+@client.event
+async def on_ready():
+    print('{0.user} is up'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.attachments:
+        file = utility.save(message.attachments[0].url)
+        name = utility.find(file, context)
+
+        if name:
+            await message.channel.send(name)
+
+client.run(TOKEN)
